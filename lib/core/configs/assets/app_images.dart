49ba920436bafd4189f8af22087b12778571312f
omer-akkoca <omerakkoca1042@gmail.com>
2025-05-07 02:20:55 +0300

    adding new assets & some widgets to use them

---
+++ lib/core/configs/assets/app_images.dart
@@ -0,0 +1,4 @@
+class AppImages{
+  static const String logoPng = "assets/images/spotify_logo.png";
+  static const String logoSvg = "assets/vectors/spotify_logo.svg";
+}
---
+++ lib/presentation/widgets/svg_asset.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class SvgAsset extends StatelessWidget {
+  final String path;
+  final double width;
+  final double height;
+  final Color? color;
+  final BoxFit? fit;
+
+  const SvgAsset({
+    super.key,
+    required this.path,
+    required this.width,
+    required this.height,
+    this.color,
+    this.fit = BoxFit.contain,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SvgPicture.asset(
+      path,
+      colorFilter: color == null ? null : ColorFilter.mode(color!, BlendMode.srcIn),
+      height: height,
+      width: width,
+      fit: fit!,
+    );
+  }
+}
