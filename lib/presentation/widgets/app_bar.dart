ad9836dda82538a6cc26c5f41cdbddd76f5fd5b6
omer-akkoca <omerakkoca1042@gmail.com>
2025-05-12 22:54:36 +0300

    new feature for app bar

--- lib/presentation/widgets/app_bar.dart
+++ lib/presentation/widgets/app_bar.dart
@@ -3,8 +3,10 @@ import 'package:spotify/common/helpers/is_dark_mode.dart';
 
 class BasicAppBar extends StatelessWidget implements PreferredSizeWidget {
   final Widget? title;
+  final bool  hideBack;
+  final List<Widget>? actions;
 
-  BasicAppBar({super.key, this.title,});
+  BasicAppBar({super.key, this.title, this.hideBack = false, this.actions});
 
   final Color darkModeBg = Colors.white.withValues(alpha: 0.03);
   final Color lightModeBg = Colors.black.withValues(alpha: 0.04);
@@ -15,23 +17,29 @@ class BasicAppBar extends StatelessWidget implements PreferredSizeWidget {
       backgroundColor: Colors.transparent,
       elevation: 0,
       centerTitle: true,
-      leading: IconButton(
-        onPressed: () => Navigator.pop(context),
-        icon: Container(
-          width: 50,
-          height: 50,
-          decoration: BoxDecoration(
-            color: context.isDarkMode ? darkModeBg : lightModeBg,
-            shape: BoxShape.circle,
-          ),
-          child: Icon(
-            Icons.arrow_back_ios_new,
-            size: 15,
-            color: context.isDarkMode ? Colors.white : Colors.black,
-          ),
+      leading: _buildLeading(context),
+      actions: actions,
+      title: title ?? Text(""),
+    );
+  }
+  
+  Widget _buildLeading(BuildContext context){
+    if (hideBack) return SizedBox();
+    return IconButton(
+      onPressed: () => Navigator.pop(context),
+      icon: Container(
+        width: 50,
+        height: 50,
+        decoration: BoxDecoration(
+          color: context.isDarkMode ? darkModeBg : lightModeBg,
+          shape: BoxShape.circle,
+        ),
+        child: Icon(
+          Icons.arrow_back_ios_new,
+          size: 15,
+          color: context.isDarkMode ? Colors.white : Colors.black,
         ),
       ),
-      title: title ?? Text(""),
     );
   }
 
